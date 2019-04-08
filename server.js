cc655794aa3e679114ed06e42f5e39cdbfaa0c6b
SebastianBukowski <seb-27@o2.pl>
2019-04-08 21:44:04 +0200

    Add mongo connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://root:<password>@cluster0-rxjya.mongodb.net/test?retryWrites=true', {
     useNewUrlParser: true
 });
 
